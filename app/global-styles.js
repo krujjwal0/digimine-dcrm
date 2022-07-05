7e814dd902396fdaa54758ea4d5ce5d772c4bda1
UJJWAL KUMAR <krujjwal0@gmail.com>
2022-07-05 22:30:59 +0530

    global style

--- app/global-styles.js
+++ app/global-styles.js
@@ -52,7 +52,7 @@ const GlobalStyle = createGlobalStyle`
   }
   .content {
     display:flex;
-    minimum-height: 96vh;
+    min-height: 96vh;
     height: 100%;
     margin-left:250px;
     border-radius: 70px 0px 0px 0px;
